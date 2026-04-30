b38f843fa1724bd3ecace046c0366c83d616f50d
ikram abbes <koukiamour5@gmail.com>
2026-05-01 00:59:56 +0300

    Final updates before deployment

--- src/App.js
+++ src/App.js
@@ -1057,7 +1057,7 @@ transition: "0.3s",
 
   <div>
     <div>
- <h2 style={{ margin: 0, fontSize: isMobile ? "12px" : "15px" }}>
+ <h2 style={{ margin: 0, fontSize: isMobile ? "12px" : "8px" }}>
   {lang === "ar"
     ? "الكلية الجامعية بتيماء"
     : "University College of Tayma"}
